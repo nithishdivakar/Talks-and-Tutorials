31bc9125792a4584492b4dbac8aecbd17b522e40
Nithish deva Divakar <NITHISH.divakar@gmail.com>
2017-06-01 18:18:22 +0530

    Update setup.sh

--- tfTalk-03June2017/setup.sh
+++ tfTalk-03June2017/setup.sh
@@ -1,10 +1,9 @@
-mkdir tfTalk-03June2017
+svn checkout https://github.com/nithishdivakar/Talks-and-Tutorials/trunk/tfTalk-03June2017                                                                     
 cd tfTalk-03June2017
-#virtualenv env --no-site-packages
-virtualenv env --no-site-packages --python=/usr/bin/python2
-. env/bin/activate
-curl -OL https://raw.githubusercontent.com/nithishdivakar/Talks-and-Tutorials/master/tfTalk-03June2017/requirements.txt
-
+# virtualenv env --no-site-packages
+virtualenv env --no-site-packages --python=$(which python2)
+source env/bin/activate
 pip install -r requirements.txt
-
+mkdir -p 05cnn/dataset/cifar
+curl -L -o 05cnn/dataset/cifar/cifar-10-python.tar.gz http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
 deactivate
